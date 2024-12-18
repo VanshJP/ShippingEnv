1f52ac3f0634e927d95886cc95b2b0005145c0ac
Kyle Lastimosa <glitchkyle16@gmail.com>
2024-12-17 20:41:32 -0500

    [refactor] remove storm temporarily

--- main.py
+++ main.py
@@ -11,19 +11,12 @@ if __name__ == "__main__":
     env.add_port([49, 72])
     env.add_port([62, 72])
 
-    env.add_storm([[20, 30], [30, 40]])
-    env.add_storm([[55, 65], [10, 20]])
-    env.add_storm([[80, 90], [60, 70]])
-    env.add_storm([[10, 20], [70, 80]])
-    env.add_storm([[40, 50], [85, 95]])
-
     state = env.reset()
 
     try:
         for _ in range(1000):
             action = env.sample_action()
             state, reward, done, meta = env.step(action)
-            env.update_storms()
             env.render_real_time()
             if done: break
     except KeyboardInterrupt:
--- shipping/environment.py
+++ shipping/environment.py
@@ -19,12 +19,6 @@ class Environment:
         self.fuel = 200
         self.origin_port_index = None
         self.destination_port_index = None
-        
-        self.storm_positions = []
-        self.storm_active = {}
-        self.storm_size = {}
-        self.storm_movement_range = {}
-        self.original_storm_positions = self.storm_positions.copy()
 
         self._initialize_map(map_path)
 
@@ -59,34 +53,6 @@ class Environment:
             return self.port_positions.pop(idx)
         else:
             raise IndexError("Invalid index")
-    
-    def add_storm(self, pos):
-        i = len(self.storm_positions)
-        self.storm_positions.append(pos)
-        self.original_storm_positions.append(pos)
-
-        if self._is_storm_on_water(pos):
-            self.storm_active[i] = random.random() < 0.3
-        else:
-            self.storm_active[i] = False  # Deactivate if not on water
-
-        self.storm_size[i] = random.randint(5, 10)
-        self.storm_movement_range[i] = (
-            (-2, 2),  # x movement range
-            (-2, 2)   # y movement range
-        )
-
-    def remove_storm(self, idx):
-        if 0 <= idx < self.storm_positions and 0 <= idx < self.original_storm_positions:
-            del self.storm_active[idx]
-            del self.storm_size[idx]
-            del self.storm_movement_range[idx]
-
-            self.storm_positions.pop(idx)
-
-            return self.original_storm_positions(idx)
-        else:
-            raise IndexError("Invalid index")
 
     def _is_within_map(self, x, y):
         """
@@ -114,77 +80,6 @@ class Environment:
         within_height = 0 <= y < self.size_game[1]
         return within_width and within_height
 
-    def _is_storm_on_water(self, storm_pos):
-        """Checks if a storm's initial position is entirely on water tiles."""
-        for x in range(storm_pos[0][0], storm_pos[0][1]):
-            for y in range(storm_pos[1][0], storm_pos[1][1]):
-                if self._is_within_map(x, y):
-                    if self.np_game[x, y] != Entity.WATER:
-                        return False
-                else:
-                    return False  # Out of bounds is considered not on water
-        return True
-
-    def _is_valid_storm_move(self, new_pos):
-        """Checks if a new storm position is entirely on water tiles."""
-        for x in range(new_pos[0][0], new_pos[0][1]):
-            for y in range(new_pos[1][0], new_pos[1][1]):
-                if self._is_within_map(x, y):
-                    if self.np_game[x, y] != Entity.WATER:
-                        return False
-                else:
-                    return False  # Out of bounds is considered invalid
-        return True
-
-    def update_storms(self):
-        temp_storm_cells = {}
-
-        for storm_index, active in self.storm_active.items():
-            if active:
-                x_range, y_range = self.storm_movement_range[storm_index]
-                dx = random.randint(x_range[0], x_range[1])
-                dy = random.randint(y_range[0], y_range[1])
-
-                new_pos = [
-                    [
-                        self.original_storm_positions[storm_index][0][0] + dx,
-                        self.original_storm_positions[storm_index][0][1] + dx,
-                    ],
-                    [
-                        self.original_storm_positions[storm_index][1][0] + dy,
-                        self.original_storm_positions[storm_index][1][1] + dy,
-                    ],
-                ]
-
-                # Keep within bounds and ensure new position is on water
-                new_pos[0][0] = max(0, min(new_pos[0][0], self.size_game[0] - self.storm_size[storm_index]))
-                new_pos[0][1] = max(0, min(new_pos[0][1], self.size_game[0] - self.storm_size[storm_index]))
-                new_pos[1][0] = max(0, min(new_pos[1][0], self.size_game[1] - self.storm_size[storm_index]))
-                new_pos[1][1] = max(0, min(new_pos[1][1], self.size_game[1] - self.storm_size[storm_index]))
-
-                # If the new position is valid (entirely on water), update the storm's position
-                if self._is_valid_storm_move(new_pos):
-                    self.storm_positions[storm_index] = new_pos
-
-                    for x in range(new_pos[0][0], new_pos[0][1]):
-                        for y in range(new_pos[1][0], new_pos[1][1]):
-                            # TODO: Possible unecessary check
-                            if self._is_within_map(x, y):
-                                temp_storm_cells[(x, y)] = True
-                else:
-                    # If the move is invalid, deactivate the storm
-                    self.storm_active[storm_index] = False
-
-        # Reset all Entity.STORM cells to Entity.WATER
-        for x in range(self.size_game[0]):
-            for y in range(self.size_game[1]):
-                if self.np_game[x, y] == Entity.STORM:
-                    self.np_game[x, y] = Entity.WATER
-
-        # Update cells that are going to be covered by storms to Entity.STORM
-        for (x, y) in temp_storm_cells:
-            self.np_game[x, y] = Entity.STORM
-
     def _calculate_fuel_cost(self, start, end):
         return calculate_euclidean_distance(start, end) * (1 + random.uniform(-0.1, 0.1))
 
@@ -193,13 +88,6 @@ class Environment:
         
         np_render[self.np_game == Entity.WATER] = Color.WATER
         np_render[self.np_game == Entity.GROUND] = Color.GROUND
-
-        for storm_index, active in self.storm_active.items():
-            if active:
-                for x in range(self.storm_positions[storm_index][0][0], self.storm_positions[storm_index][0][1]):
-                    for y in range(self.storm_positions[storm_index][1][0], self.storm_positions[storm_index][1][1]):
-                        if self._is_within_map(x, y):
-                            np_render[x, y] = Color.STORM
 
         np_render[self.np_game == Entity.PORT] = Color.PORT
         np_render[self.np_game == Entity.DESTINY_PORT] = Color.DESTINY_PORT
@@ -234,15 +122,6 @@ class Environment:
             cv2.destroyAllWindows()
             exit()
 
-
-    def _is_in_storm(self, x, y):
-        for storm_index, active in self.storm_active.items():
-            if active:
-                if (self.storm_positions[storm_index][0][0] <= x < self.storm_positions[storm_index][0][1] and 
-                    self.storm_positions[storm_index][1][0] <= y < self.storm_positions[storm_index][1][1]):
-                    return True
-        return False
-    
     def _is_ship_at_port(self):
         """
         Determines which port ship is currently on and returns the index of that port.
@@ -389,16 +268,6 @@ class Environment:
         if self.fuel < fuel_cost:
             reward -= 10
             done = True
-
-        # ** Penalize ship if within storm [remove this if storm is no longer considered]
-        if self._is_in_storm(new_x, new_y):
-            reward -= 5
-            if random.random() < 0.03:
-                self.cargo = 0
-                reward -= 50
-                print("Lost all cargo in the storm!")
-            if random.random() < 0.05:
-                move_x, move_y = 0, 0
 
         # ** Manuvering process
         if self.np_game[new_x, new_y] == Entity.GROUND:
